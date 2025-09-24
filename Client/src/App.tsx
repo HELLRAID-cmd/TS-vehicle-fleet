70dc8a7bb1dd465949c91a141a5d5a045abdffad
HELLRAID-cmd <emil.gaynulin99@bk.ru>
2025-09-24 21:34:13 +0600

    Импортировал файлы

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,9 +1,13 @@
-function App() {
+import "./styles/_reset.scss";
+import "./styles/_container.scss";
+import { CardPark } from "./components/CardPark/CardPark";
 
+function App() {
   return (
     <>
+      <CardPark />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
